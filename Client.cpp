e054ae1726d3ff3ac2796b07815479d058e45f06
martinnsvobodaa <63460845+martinnsvobodaa@users.noreply.github.com>
2020-04-11 11:53:03 +0200

    commit

---
+++ Client.cpp
@@ -0,0 +1,26 @@
+#include "Client.h"
+#include <string>
+
+int Client::objectsCount = 0;
+
+Client::Client(int c, string n) {
+	this->code = c;
+	this->name = n;
+	Client::objectsCount += 1;
+}
+
+Client::~Client(){
+	Client::objectsCount -= 1;
+}
+
+int Client::GetCode() {
+	return this->code;
+}
+
+string Client::GetName() {
+	return this->name;
+}
+
+int Client::GetObjectsCount() {
+	return Client::objectsCount;
+}
